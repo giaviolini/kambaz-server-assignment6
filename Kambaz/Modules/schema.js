5b3eae32e0624a4131fc0c65a18a62ca8da53ed9
Gia Violini <g1aviolini123@gmail.com>
2025-12-12 11:53:46 -0500

    modules schema

--- Kambaz/Modules/schema.js
+++ Kambaz/Modules/schema.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-const schema = new mongoose.Schema(
-  {
+const schema = new mongoose.Schema({
     _id: String,
     name: String,
     description: String,
+    course: { type: String, ref: "CourseModel", required: true }, // ADD THIS
     lessons: [{ _id: String, name: String, description: String }]
-  }
-);
+});
 export default schema;
